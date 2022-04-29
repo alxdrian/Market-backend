2c9035e118c1bd5edc8d8682bce1d7b550cbb620
adelacruz <adelacruz@canvia.com>
2022-04-29 02:54:57 -0500

    create route for products

--- src/app.js
+++ src/app.js
@@ -11,7 +11,19 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 // Routes
+app.use("/api/products", require("./routes/productRoutes"));
 
 // Error Handler
+app.use((err, req, res, next) => {
+  console.log(err.status);
+  console.log(err.message);
+
+  res.status(err.status || 500);
+  res.json({
+    error: err,
+    message: err.message
+  });
+});
+
 
 module.exports = app;
