31d997b7f5c6a9022a53da44750b36010aaa5b2e
adelacruz <adelacruz@canvia.com>
2022-04-29 05:01:13 -0500

    config cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.set("port", process.env.PORT || 4000);
 // Middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 // Routes
 app.use("/api/products", require("./routes/productRoutes"));
